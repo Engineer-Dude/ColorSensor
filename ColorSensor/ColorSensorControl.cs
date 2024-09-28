be83e518208a980bf520401a2a7833d0af3c3d0f
Engineer-Dude <paulphillips@phillipsengineer.com>
2024-09-28 10:56:57 -0700

    Initial commit.

---
+++ ColorSensor/ColorSensorControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ColorSensor
+{
+    public class ColorSensorControl : Control
+    {
+        public bool ShowValue
+        {
+            get { return (bool)GetValue(ShowValueProperty); }
+            set { SetValue(ShowValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShowValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowValueProperty =
+            DependencyProperty.Register("ShowValue", typeof(bool), typeof(ColorSensorControl), new PropertyMetadata(true));
+
+        static ColorSensorControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorSensorControl), new FrameworkPropertyMetadata(typeof(ColorSensorControl)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            TextBlock redValue = Template.FindName("PART_RedValue", this) as TextBlock ?? new TextBlock();
+            TextBlock greenValue = Template.FindName("PART_GreenValue", this) as TextBlock ?? new TextBlock();
+            TextBlock blueValue = Template.FindName("PART_BlueValue", this) as TextBlock ?? new TextBlock();
+            TextBlock whiteValue = Template.FindName("PART_WhiteValue", this) as TextBlock ?? new TextBlock();
+
+            Binding showValueBinding = new Binding
+            {
+                Path = new PropertyPath(nameof(ShowValue)),
+                Source = this,
+                Converter = new BooleanToVisibilityConverter()
+            };
+
+            redValue.SetBinding(VisibilityProperty, showValueBinding);
+            greenValue.SetBinding(VisibilityProperty, showValueBinding);
+            blueValue.SetBinding(VisibilityProperty, showValueBinding);
+            whiteValue.SetBinding(VisibilityProperty, showValueBinding);
+
+            base.OnApplyTemplate();
+        }
+    }
+}
