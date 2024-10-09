2e406d42fc9c2c60f270a4926a60ce02bb2ef3d7
Engineer-Dude <paulphillips@phillipsengineer.com>
2024-10-09 15:07:08 -0700

    Added width scaling.
    Added folder ColorSensor/Converters/
    Added IValueConverter called ValueDivisionConverter.cs
    
    In ColorSensor/ColorSensorStyle.xaml:
     o Added namespace for the converter.
     o Added StaticResource for ValueDivisionConverter
     o Gave an x:Name= TheBorder to the Border.  It will be used for scaling the things below.
     o Changed the Grid RowDefinitions and ColumnDefinitions to auto.  This will allow for the items to scale.
     o Added scaling to the Ellipse, Label, and TextBox for the four colors
    
    In MainWindow.xaml:
     o Changed the Grid RowDefinitions and ColumnDefinitions.

---
+++ ColorSensor/Converters/ValueDivisionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ColorSensor.Converters
+{
+    public class ValueDivisionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is not null &&
+                double.TryParse(value.ToString(), out double val) &&
+                double.TryParse(parameter.ToString(), out double param) ? val / param : double.NaN;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
