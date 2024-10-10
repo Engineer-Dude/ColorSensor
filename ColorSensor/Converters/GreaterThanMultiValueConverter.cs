9ba148b6c2f7d06200ecc4947879c21588ef0422
Engineer-Dude <paulphillips@phillipsengineer.com>
2024-10-10 15:01:18 -0700

    Added GreaterThanMultiValueConverter and implemented it ColorSensor/ColorSensorStyle for the ellipse.

---
+++ ColorSensor/Converters/GreaterThanMultiValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ColorSensor.Converters
+{
+    public class GreaterThanMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+
+            result = values is not null && values.Length == 2 &&
+                double.TryParse(values[0].ToString(), out double value1) &&
+                double.TryParse(values[1].ToString(), out double value2) &&
+                value1 > value2;
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
